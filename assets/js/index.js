2548e6a5b905f5b6ba0c5997f09355434d938d90
sangeethaNR <sangeethajadhav123@gmail.com>
2021-08-04 15:11:06 -0700

    added questions and function to call the inquirer

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,35 @@ const questions = ['Enter your Project Title','Provide a short description expla
                  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
 
 
+    
+        const responseObj = JSON.parse(data);
+        const filetext =`<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Document</title>
+          </head>
+          <body>
+           <h1> Welcome ${data.name}</h1>
+        <ul>
+        <li> Your location : ${data.description}</li>
+        <li> Your Bio : ${data.install}</li>
+        <li> <a href='${responseObj.example}' target="_blank">LinkedIn</a><li>
+        <li> <a href='${responseObj.license}' target="_blank"> Github</li>
+        </ul>  
+            
+         </body>
+          </html>`
+          fs.writeFile('index.html', filetext);
+          return ;
+        
+       
+          
 }
 
 // TODO: Create a function to initialize app
@@ -39,15 +65,22 @@ function init() {
 
       },
       {
-        type: 'input',
+        type: 'list',
         message: questions[4],
         name: 'license',
+        choices:['MIT','GNU','Apache 2.0'],
       }
 
     ])
+    .then((response) =>
+        //console.log(JSON.stringify(response));
+   //console.log(JSON.stringify(response));
+   fs.writeFile('README.md', writeToFile(JSON.stringify(response)), (err) =>
+   err ? console.error(err) : console.log('you have successfully saved the info!')
+ 
+  ));
+    }
 
-
-}
 
 // Function call to initialize app
 init();
